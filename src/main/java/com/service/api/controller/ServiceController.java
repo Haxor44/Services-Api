ceae987e4f922229ff9ecbc40585729ec28aff66
Haxor44 <evolmalek04@gmail.com>
2023-07-28 22:46:04 +0300

    final updates

--- src/main/java/com/service/api/controller/ServiceController.java
+++ src/main/java/com/service/api/controller/ServiceController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v2")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000",allowCredentials="true")
 @RequiredArgsConstructor
 public class ServiceController {
     private final ViewsServices viewsServices;
