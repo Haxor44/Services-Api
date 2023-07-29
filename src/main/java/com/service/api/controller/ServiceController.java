8d35526a93dea04da294a81fe646bdd29679daa9
Haxor44 <evolmalek04@gmail.com>
2023-07-29 21:10:50 +0300

    it works

--- src/main/java/com/service/api/controller/ServiceController.java
+++ src/main/java/com/service/api/controller/ServiceController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v2")
-@CrossOrigin(origins = "http://localhost:3000",allowCredentials="true")
+@CrossOrigin(origins = "https://roman-frontend.vercel.app/",allowCredentials="true")
 @RequiredArgsConstructor
 public class ServiceController {
     private final ViewsServices viewsServices;
